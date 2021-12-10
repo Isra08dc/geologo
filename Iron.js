a6d78e4ebec1540797085b07eb4242a793738aa0
Isra08dc <90532141+Isra08dc@users.noreply.github.com>
2021-12-09 18:50:18 -0600

    Add files via upload

--- Iron.js
+++ Iron.js
@@ -2,27 +2,29 @@ class Iron{
 	constructor(x,y,r)
 	{
 	var options={
-	restitution:0.8,
-	friction: 3,
-	density: 30
+	'restitution':0.8,
+	'friction': 3,
+	'density': 30
 	}
-		this.x=x;
-		this.y=y;
-		this.r=r
-		World.add(world, this.body);
+	this.body = Bodies.rectangle(x, y, 50, 50, options);
+    this.width = 150;
+    this.height = 40;
+    World.add(world, this.body);
+  };
+  display(){
+    var pos = this.body.position;
+    pos.x = mouseX;
+    pos.y = mouseY;
+    var angle = this.body.angle;
 
-	}
-	display()
-	{
-			var ironpos=this.body.position;		
-			push()
-			translate(ironpos.x, ironpos.y);
-			rectMode(CENTER)
-			strokeWeight(4);
-			stroke("black");
-			fill("darkblue");
-			
-			pop()
-	}
-
-}
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    strokeWeight(3);
+    stroke('white')
+    fill('orange')
+    rectMode(CENTER)
+    rect(0, 0, this.width, this.height);
+    pop();
+  };
+};
