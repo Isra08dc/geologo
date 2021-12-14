05670c97a8e18f732f11d45c5276b793e6608ba2
Isra08dc <90532141+Isra08dc@users.noreply.github.com>
2021-12-14 17:54:55 -0600

    Add files via upload

--- Stone.js
+++ Stone.js
@@ -1,5 +1,5 @@
 class Stone{
-	constructor(x,y,r)
+	constructor(x, y, width, height)
 	{
 	var options={
     restitution:0.8,
@@ -9,7 +9,7 @@ class Stone{
 		this.x=x;
 		this.y=y;
 		this.r=r
-		this.body=Bodies.circle(this.x, this.y, (this.r-20)/2, options)
+		this.body=Bodies.rectangle(this.x, this.y, (this.r-20)/2, options)
 		World.add(world, this.body);
 
 	}
